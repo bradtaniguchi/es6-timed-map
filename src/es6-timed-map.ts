b7e73162a6ed0377c04a9826b646985e4b27c18e
brad taniguchi <bradtaniguchi@gmail.com>
2020-10-01 23:33:44 -0700

    Started adding actual logic

--- src/es6-timed-map.ts
+++ src/es6-timed-map.ts
@@ -7,6 +7,16 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
  */
 export default class Es6TimedMap<K, V, E> {
+  /**
+   * The core map data, used to keep track of the data
+   */
+  private _core = new Map<K, V>();
+  /**
+   * A map of timers where the key is the same key provided
+   * to the `_core` map.
+   * TODO: update timer value
+   */
+  private _timers = new Map<K, number>();
   constructor(private _entries?: readonly [K, V, E] | null) {}
 
   /**
@@ -16,10 +26,11 @@ export default class Es6TimedMap<K, V, E> {
     return this.entries?.length || 0;
   }
   /**
-   * Removes all key-value pairs from the `TimedMap` object
+   * Removes all key-value pairs from the `TimedMap` object.
    */
   public clear(): void {
-    // TODO:
+    this._core.clear();
+    this._timers.clear();
   }
 
   /**
@@ -28,8 +39,7 @@ export default class Es6TimedMap<K, V, E> {
    * @param key the key to remove
    */
   public delete(key: K): boolean {
-    // TODO:
-    return false;
+    return this._core.delete(key);
   }
 
   public forEach(
