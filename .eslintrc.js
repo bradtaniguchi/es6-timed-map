996e9f4ab27380ebc73411efb335458642e202bf
David Tran <davidlamt@users.noreply.github.com>
2020-10-04 14:57:18 -0700

    Integrate Prettier with ESLint and other small cleanups

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
+  env: {
+    node: true
+  },
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-  ],
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ]
 };
